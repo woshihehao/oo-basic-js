a49a3460aa250c1e240e9ca77b5a8d4b624f5a99
hackeris <hackeris@qq.com>
2015-09-12 18:50:30 +0800

    fix example spec bug

--- example/spec/main-spec.js
+++ example/spec/main-spec.js
@@ -6,7 +6,7 @@ describe('Math function test', function () {
         expect(sum).toBe(7);
     });
 
-    it('function add should return the sum of a,b', function () {
+    it('function mul should return the sum of a,b', function () {
 
         var sum = mul(3, 4);
         expect(sum).toBe(12);
