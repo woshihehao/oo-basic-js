d8f06bb0632458f4d4add7b91a7d70b2339a0dfd
hehao <2791566231@qq.com>
2015-09-15 13:10:20 +0800

    first

--- practice_v01/src/main.js
+++ practice_v01/src/main.js
@@ -1,1 +1,8 @@
-//  Your code here
+function Person(age,name){
+    this.age = age;
+    this.name = name ;
+    this.introduce = function(){
+
+        return 'My name is ' +this.name+ '. I am ' +this.age+ ' years old.';
+    };
+}
